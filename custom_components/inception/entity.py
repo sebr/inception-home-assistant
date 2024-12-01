85015ddfe5c20da232a53478d2ccac8a3b164f4a
Seb Ruiz <seb.ruiz@calven.com>
2024-12-01 04:53:36 +0000

    Add support for doors

--- custom_components/inception/entity.py
+++ custom_components/inception/entity.py
@@ -2,17 +2,8 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
-from typing import TYPE_CHECKING, Callable
+from typing import TYPE_CHECKING
 
-from homeassistant.components.binary_sensor import (
-    DOMAIN as BINARY_SENSOR_DOMAIN,
-)
-from homeassistant.components.binary_sensor import (
-    BinarySensorDeviceClass,
-    BinarySensorEntity,
-    BinarySensorEntityDescription,
-)
 from homeassistant.core import callback
 from homeassistant.helpers.device_registry import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
@@ -27,9 +18,8 @@ if TYPE_CHECKING:
 
 
 class InceptionEntity(CoordinatorEntity[InceptionUpdateCoordinator]):
-    """Entity class for Hydrawise devices."""
+    """Entity class for Inception devices."""
 
-    _attr_attribution = "Data provided by hydrawise.com"
     _attr_has_entity_name = True
 
     def __init__(
@@ -42,14 +32,16 @@ class InceptionEntity(CoordinatorEntity[InceptionUpdateCoordinator]):
         """Initialize the Hydrawise entity."""
         super().__init__(coordinator=coordinator)
         self.entity_description = description
-        # self._device_id = inception_object.ID
+        self._attr_attribution = f"Data provided by {coordinator.api._host}"  # noqa: SLF001
         self._attr_unique_id = inception_object.ID
+        # self._device_id = inception_object.ID
         # self._attr_device_info = DeviceInfo(
         #     identifiers={(DOMAIN, self._device_id)},
         #     name=inception_object.Name,
         #     manufacturer=MANUFACTURER,
         # )
         self._inception_object = inception_object
+        self._attr_extra_state_attributes = inception_object.extra_fields
         self._update_attrs()
 
     def _update_attrs(self) -> None:
